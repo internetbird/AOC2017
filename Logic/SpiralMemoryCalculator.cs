d4c6e17ac3f2a9d3103e27b79eeb0407c034a3bc
Shai Aharoni <saharoni@gmail.com>
2021-08-24 22:02:18 +0300

    Day 3 - Start

---
+++ Logic/SpiralMemoryCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC2017.Logic
+{
+    public class SpiralMemoryCalculator
+    {
+
+        public int GetSpiralIndexForLocation(int location)
+        {
+
+            int index = 0;
+            int currIndexLocationStart = 1, currIndexLocationEnd = 1;
+
+            while (!(location >= currIndexLocationStart && location <= currIndexLocationEnd))
+            {
+                index++;
+                currIndexLocationStart = currIndexLocationEnd + 1;
+                int currSpiralSideLength = (index * 2 + 1);
+                currIndexLocationEnd = currIndexLocationStart + (currSpiralSideLength * 2) + (currSpiralSideLength - 2) * 2 - 1;
+            }
+
+            return index;
+        }
+    }
+}
---
+++ PuzzleSolvers/Day3PuzzleSolver.cs
@@ -0,0 +1,24 @@
+﻿using AOC2017.Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC2017.PuzzleSolvers
+{
+    public class Day3PuzzleSolver : IPuzzleSolver
+    {
+        public string SolvePuzzlePart1()
+        {
+            var calculator = new SpiralMemoryCalculator();
+
+            int index =  calculator.GetSpiralIndexForLocation(22);
+
+            return index.ToString();
+        }
+
+        public string SolvePuzzlePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
