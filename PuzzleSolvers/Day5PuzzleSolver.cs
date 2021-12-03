8e909c5db9274a2eea3e7c1f84c501d2c50bc7da
Shai Aharoni <saharoni@gmail.com>
2021-12-03 21:17:27 +0200

    day 5

---
+++ PuzzleSolvers/Day5PuzzleSolver.cs
@@ -0,0 +1,26 @@
+﻿using AOC2017.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AOC2017.PuzzleSolvers
+{
+    public class Day5PuzzleSolver : IPuzzleSolver
+    {
+        public string SolvePuzzlePart1()
+        {
+            string[] inputLines = InputFilesHelper.GetInputFileLines("day5.txt");
+
+            int[] instructions = inputLines.Select(line => int.Parse(line)).ToArray();
+
+
+            return string.Empty;
+        }
+
+        public string SolvePuzzlePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
