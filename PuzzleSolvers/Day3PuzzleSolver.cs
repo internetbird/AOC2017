4c33ce5f50a3b4fd26d15b040a50c212db01b880
Shai Aharoni <saharoni@gmail.com>
2021-12-04 19:53:22 +0200

    Day 3 - Part 2 (Thanks to darshanrampatel/Advent-of-Code-2017)

--- PuzzleSolvers/Day3PuzzleSolver.cs
+++ PuzzleSolvers/Day3PuzzleSolver.cs
@@ -1,6 +1,8 @@
 ﻿using AOC2017.Logic;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using System.Text;
 
 namespace AOC2017.PuzzleSolvers
@@ -20,10 +22,98 @@ namespace AOC2017.PuzzleSolvers
 
             return manhattenDistance.ToString();
         }
+        enum Directions
+        {
+            Right = 0,
+            Up = 1,
+            Left = 2,
+            Down = 3
+        }
 
         public string SolvePuzzlePart2()
         {
-            throw new NotImplementedException();
+
+            const int input = 312051;
+
+            var currentPosition = new Point(0, 0);
+            var currentValue = 1;
+            var currentDirection = Directions.Down;//since the next direction is Right
+            var currentCount = 0;
+
+            var positions = new Dictionary<Point, int>
+            {
+                { currentPosition, currentValue }
+            };
+            currentValue++;
+            while (currentValue < input)
+            {
+                var (nextDirection, nextCount) = GetNextStep(currentDirection, currentCount);
+                currentDirection = nextDirection;
+                currentCount = nextCount;
+
+                while (nextCount > 0 && currentValue < input)
+                {
+                    currentPosition = GetNextPosition(currentPosition, nextDirection);
+                    currentValue = GetAdjacentSum(currentPosition, positions);
+                    positions.Add(currentPosition, currentValue);
+                    nextCount--;
+                }
+            }
+            return currentValue.ToString();
+        }
+
+        private  (Directions nextDirection, int nextCount) GetNextStep(Directions currentDirection, int currentCount)
+        {
+            switch (currentDirection)
+            {
+                case Directions.Right:
+                    return (Directions.Up, currentCount);
+                case Directions.Up:
+                    return (Directions.Left, currentCount + 1);
+                case Directions.Left:
+                    return (Directions.Down, currentCount);
+                case Directions.Down:
+                    return (Directions.Right, currentCount + 1);
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private  Point GetNextPosition(Point position, Directions direction)
+        {
+            switch (direction)
+            {
+                case Directions.Right:
+                    position.X++;
+                    return position;
+                case Directions.Up:
+                    position.Y++;
+                    return position;
+                case Directions.Left:
+                    position.X--;
+                    return position;
+                case Directions.Down:
+                    position.Y--;
+                    return position;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private static int GetAdjacentSum(Point currentPosition, Dictionary<Point, int> positions)
+        {
+            var topLeft = positions.FirstOrDefault(p => p.Key == new Point(currentPosition.X - 1, currentPosition.Y - 1));
+            var topCentre = positions.FirstOrDefault(p => p.Key == new Point(currentPosition.X, currentPosition.Y - 1));
+            var topRight = positions.FirstOrDefault(p => p.Key == new Point(currentPosition.X + 1, currentPosition.Y - 1));
+            var left = positions.FirstOrDefault(p => p.Key == new Point(currentPosition.X - 1, currentPosition.Y));
+            var right = positions.FirstOrDefault(p => p.Key == new Point(currentPosition.X + 1, currentPosition.Y));
+            var bottomLeft = positions.FirstOrDefault(p => p.Key == new Point(currentPosition.X - 1, currentPosition.Y + 1));
+            var bottomCentre = positions.FirstOrDefault(p => p.Key == new Point(currentPosition.X, currentPosition.Y + 1));
+            var bottomRight = positions.FirstOrDefault(p => p.Key == new Point(currentPosition.X + 1, currentPosition.Y + 1));
+
+            return topLeft.Value + topCentre.Value + topRight.Value + left.Value + right.Value + bottomLeft.Value + bottomCentre.Value + bottomRight.Value;
         }
     }
 }
+
+
