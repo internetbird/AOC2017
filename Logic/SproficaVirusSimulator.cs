2c12d8e92caeb29a0eb8a3bda2aee0ba0edeef48
Shai Aharoni <saharoni@gmail.com>
2022-12-04 10:12:36 +0200

    Day 22 - Part 1

---
+++ Logic/SproficaVirusSimulator.cs
@@ -0,0 +1,86 @@
+﻿using BirdLib.DataModels;
+using BirdLib.Enums;
+using BirdLib.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2017.Logic
+{
+    public class SproficaVirusSimulator
+    {
+        private Grid<bool> _nodesNetwork;
+        private int _currentRowIndex;
+        private int _currentColumnIndex;
+        private Direction _currentDirection;
+        private int _infectionsCounter;
+        private const int NetworkSize = 125;
+
+
+        public int RunSimulation(Grid<bool> initialGrid, int numOfBursts)
+        {
+            int burstCounter = 0;
+
+            _infectionsCounter = 0;
+
+            InitNodesNetwork(initialGrid);
+            _currentRowIndex = NetworkSize / 2;
+            _currentColumnIndex = NetworkSize / 2;
+            _currentDirection = Direction.Up;
+
+            while (burstCounter < numOfBursts)
+            {
+                bool isCurrentNodeInfected = _nodesNetwork.GetItem(_currentRowIndex, _currentColumnIndex);
+
+
+                if (isCurrentNodeInfected)
+                {
+                    _currentDirection = _currentDirection.TurnRight();
+                    _nodesNetwork.SetItem(false, _currentRowIndex, _currentColumnIndex);
+                }
+                else
+                {
+                    _currentDirection = _currentDirection.TurnLeft();
+                    _nodesNetwork.SetItem(true, _currentRowIndex, _currentColumnIndex);
+                    _infectionsCounter++;
+                }
+
+                MoveToNextNode();
+                burstCounter++;
+           
+            }
+            return _infectionsCounter;
+
+        }
+
+        private void InitNodesNetwork(Grid<bool> initialGrid)
+        {
+            _nodesNetwork = new Grid<bool>(NetworkSize, NetworkSize);
+            _nodesNetwork.SetSubGrid((NetworkSize-25)/2, (NetworkSize-25)/2, initialGrid);
+        }
+
+        private void MoveToNextNode()
+        {
+           switch(_currentDirection)
+            {
+                case Direction.Up:
+                    _currentRowIndex--;
+                    break;
+                case Direction.Down:
+                    _currentRowIndex++;
+                    break;
+                case Direction.Right:
+                    _currentColumnIndex++;
+                    break;
+                case Direction.Left:
+                    _currentColumnIndex--;
+                    break;
+
+            }
+        }
+    }
+}
---
+++ PuzzleSolvers/Day22PuzzleSolver.cs
@@ -0,0 +1,51 @@
+﻿using AOC;
+using AOC2017.Logic;
+using BirdLib.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2017.PuzzleSolvers
+{
+    public class Day22PuzzleSolver : IPuzzleSolver
+    {
+        public string SolvePuzzlePart1()
+        {
+            Grid<bool> initialNetwork = GetInitialNetwork();
+
+            var virusSimulator = new SproficaVirusSimulator();
+
+            int numOfInfections = virusSimulator.RunSimulation(initialNetwork, 10000);
+
+            return numOfInfections.ToString();
+        }
+
+        private Grid<bool> GetInitialNetwork()
+        {
+            int networkInitialSize = 25;
+            var network = new Grid<bool>(networkInitialSize, networkInitialSize);
+
+            string[] inputLines = InputFilesHelper.GetInputFileLines("day22.txt");
+
+            for (int i = 0; i < networkInitialSize; i++)
+            {
+
+                string currLine = inputLines[i];
+
+                for (int j = 0; j < networkInitialSize; j++)
+                {
+                    bool infected = currLine[j] == '#';
+                    network.SetItem(infected, i, j);
+                }
+            }
+            return network;
+        }
+
+        public string SolvePuzzlePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
