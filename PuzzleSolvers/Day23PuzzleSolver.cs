ffaf015dddff5e1b7b49013a8d452392f15c53d7
Shai Aharoni <saharoni@gmail.com>
2022-12-18 12:20:34 +0200

    Day 23 - Part 2

--- PuzzleSolvers/Day23PuzzleSolver.cs
+++ PuzzleSolvers/Day23PuzzleSolver.cs
@@ -1,6 +1,7 @@
 ﻿using AOC;
 using AOC2017.Logic;
 using AOC2017.Parsers;
+using BirdLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,25 +27,23 @@ namespace AOC2017.PuzzleSolvers
 
         public string SolvePuzzlePart2()
         {
-            string[] programLines = InputFilesHelper.GetInputFileLines("day23.test.txt");
+            // Temporary hardcoded solution
+            // Mus take maximum value of b (108100 in my case)
+            // and increments it by value of second-to-last instruction
+            // up to maximum value of c,
+            // and compute the total number of composite values.
+            // `1001 - sum(map(pyprimes.isprime, range(108100, 125100, 17)))`
 
-            var parser = new DuetComputerInstructionParser();
-            var computer = new CoprocessorComputer(parser);
+            var count = 0;
 
-            computer.LoadProgram(programLines);
-
-
-            computer.OnAfterInstructionExecuted += (sender, args) =>
+            for (int i = 108100; i <= 125100; i+=17)
             {
-                if (computer.GetResultRegisterValue() != 0)
+                if (i.IsPrime())
                 {
-                    Console.WriteLine("Register h equals :" + computer.GetResultRegisterValue());
+                    count++;
                 }
-            };
-
-            computer.ExcuteProgram();
-
-            return computer.GetResultRegisterValue().ToString();
+            }
+            return (1001 - count).ToString() ;
         }
     }
 }
