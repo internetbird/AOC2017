4b50a1b8e50cc53c470cb68359f89eb26c006fae
Shai Aharoni <saharoni@gmail.com>
2022-12-18 12:21:14 +0200

    Day 23 - Part 2

--- PuzzleSolvers/Day23PuzzleSolver.cs
+++ PuzzleSolvers/Day23PuzzleSolver.cs
@@ -27,12 +27,12 @@ namespace AOC2017.PuzzleSolvers
 
         public string SolvePuzzlePart2()
         {
-            // Temporary hardcoded solution
-            // Mus take maximum value of b (108100 in my case)
+          
+            // Take maximum value of b (108100 in my case)
             // and increments it by value of second-to-last instruction
             // up to maximum value of c,
             // and compute the total number of composite values.
-            // `1001 - sum(map(pyprimes.isprime, range(108100, 125100, 17)))`
+            // `1001 -  number of prime number in the range(108100, 125100, 17)))`
 
             var count = 0;
 
